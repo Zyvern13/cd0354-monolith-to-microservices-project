d417ce18d76646668f8cddc98a980f7b38f84042
Zyvern13 <95839181+Zyvern13@users.noreply.github.com>
2026-08-23 18:10:39 +0000

    Refactor monolith to microservices with Docker and CI setup

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  // apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'https://turbo-palm-tree-r5gwpxq49rr2xwxr-8080.app.github.dev/api/v0'
 };
 
 /*
