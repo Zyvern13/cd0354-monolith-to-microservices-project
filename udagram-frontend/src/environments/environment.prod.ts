ca7bae98481e340751265fc62a139cc549e53413
Zyvern13 <95839181+Zyvern13@users.noreply.github.com>
2026-08-24 04:08:04 +0000

    Add Kubernetes deployment manifests and screenshots

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://REVERSE_PROXY_EXTERNAL_IP:8080/api/v0'
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  // apiHost: 'http://localhost:8080/api/v0'
+  // apiHost: 'http://REVERSE_PROXY_EXTERNAL_IP:8080/api/v0'
   apiHost: 'https://turbo-palm-tree-r5gwpxq49rr2xwxr-8080.app.github.dev/api/v0'
 };
 
